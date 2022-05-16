af03c41e04e25045ee6195a21cd614002e856b76
Olga Vasylevskaya <olgavasylevskaya@ukr.net>
2022-05-16 15:28:48 +0200

    fix converter

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,6 @@ import './styles/index.scss'
 import Header from './components/Header'
 import Converter from './components/Converter'
 
-import { getLatestCurrencyValues, exchangeCurrency } from './api/api'
-import allowedValues from './config/allowedValues'
-
-
-const { UAH, EUR, USD } = allowedValues
-
-getLatestCurrencyValues('USD', ['UAH', 'EUR'])
-
 function App() {
   return (
     <div className="App">
